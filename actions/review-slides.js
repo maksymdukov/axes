8c30449201e53c7b92ab157443d208e986590c2c
Maksym Dukov <maxopowerful@gmail.com>
2020-06-01 19:51:36 +0300

    - retrieval of review-slides fix. super-weird behaviour

--- actions/review-slides.js
+++ actions/review-slides.js
@@ -2,13 +2,14 @@ import { client, locales } from '../server/config/contentful';
 import { normalizeReviewSlides } from './review-slides.utils';
 
 export const getReviewSlideEntries = (options) =>
-  client.getEntry({
-    entry_id: '4K2pBRoR3sHFCPXfgaxaJ2',
+  client.getEntries({
+    content_type: 'reviewSlides',
+    'sys.id': '4K2pBRoR3sHFCPXfgaxaJ2',
     locale: locales.ua,
     ...options
   });
 
 export const getReviewSlides = async () => {
-  const slides = await getReviewSlideEntries();
-  return normalizeReviewSlides(slides);
+  const slides = await getReviewSlideEntries({});
+  return normalizeReviewSlides(slides.items[0] || []);
 };
