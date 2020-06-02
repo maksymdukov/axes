e465927d05b3a2ac808c56e19b48a4fae24e53da
Maksym Dukov <maxopowerful@gmail.com>
2020-06-02 09:04:34 +0300

    - thinner fontweight in rechtext paragraph

--- components/shared/richtext-page/paragraph.js
+++ components/shared/richtext-page/paragraph.js
@@ -3,11 +3,16 @@ import { Typography, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles(({ spacing }) => ({
   paragraph: {
-    marginBottom: spacing(2)
+    marginBottom: spacing(2),
+    fontWeight: 300
   }
 }));
 
 export const renderParagraph = (node, children) => {
   const classes = useStyles();
-  return <Typography className={classes.paragraph}>{children}</Typography>;
+  return (
+    <Typography className={classes.paragraph} variant="body1">
+      {children}
+    </Typography>
+  );
 };
