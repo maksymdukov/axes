d3681ba282f5f2557707d3e4dec0e9dbe21c600b
Maksym Dukov <maxopowerful@gmail.com>
2020-06-07 18:00:18 +0300

    - refactor get-axes serverless endpoint

--- pages_/api/get-axes.js
+++ pages_/api/get-axes.js
@@ -1,51 +1,7 @@
-import Validator from 'fastest-validator';
 import { getAxes } from '~/actions/axe';
-import config from '~/server/config/contentful';
+import contentfulConfig from '~/server/config/contentful';
 import { AXES_SORT } from '~/actions/axe.constants';
-
-const SORT = {
-  createdAt: 'createdAt',
-  price: 'price'
-};
-const ORDER = {
-  asc: 'asc',
-  desc: 'desc'
-};
-
-const v = new Validator();
-
-const schema = {
-  order: {
-    type: 'enum',
-    values: [ORDER.asc, ORDER.desc],
-    default: ORDER.desc
-  },
-  sort: {
-    type: 'enum',
-    values: [SORT.createdAt, SORT.price],
-    default: SORT.createdAt
-  },
-  lang: {
-    type: 'enum',
-    values: [config.locales.ru, config.locales.ua],
-    default: config.locales.ru
-  },
-  page: {
-    type: 'number',
-    min: 1,
-    default: 1,
-    integer: true,
-    convert: true
-  },
-  size: {
-    type: 'number',
-    min: 1,
-    max: 999,
-    default: 10,
-    integer: true,
-    convert: true
-  }
-};
+import { schema, v } from '~/server/validation/get-axes.validators';
 
 export default async (req, res) => {
   if (req.method !== 'GET') {
@@ -63,7 +19,7 @@ export default async (req, res) => {
     page,
     size,
     sort: AXES_SORT[sort],
-    sortOrder: config.C_SORT_ORDER[order]
+    sortOrder: contentfulConfig.C_SORT_ORDER[order]
   });
 
   res.json(axes);
---
+++ server/validation/get-axes.validators.js
@@ -0,0 +1,48 @@
+import Validator from 'fastest-validator';
+import contentfulConfig from '~/server/config/contentful';
+
+const { locales } = contentfulConfig;
+
+const SORT = {
+  createdAt: 'createdAt',
+  price: 'price'
+};
+const ORDER = {
+  asc: 'asc',
+  desc: 'desc'
+};
+
+export const v = new Validator();
+
+export const schema = {
+  order: {
+    type: 'enum',
+    values: [ORDER.asc, ORDER.desc],
+    default: ORDER.desc
+  },
+  sort: {
+    type: 'enum',
+    values: [SORT.createdAt, SORT.price],
+    default: SORT.createdAt
+  },
+  lang: {
+    type: 'enum',
+    values: [locales.ru, locales.ua],
+    default: locales.ru
+  },
+  page: {
+    type: 'number',
+    min: 1,
+    default: 1,
+    integer: true,
+    convert: true
+  },
+  size: {
+    type: 'number',
+    min: 1,
+    max: 999,
+    default: 10,
+    integer: true,
+    convert: true
+  }
+};
